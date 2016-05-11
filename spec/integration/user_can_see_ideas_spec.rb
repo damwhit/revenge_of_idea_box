1aacee0f20f1f73bb42077e4d6f4dd3af80826e0
David Whitaker <damwhit@gmail.com>
2016-05-10 18:20:08 -0600

    all tests passing

--- spec/integration/user_can_see_ideas_spec.rb
+++ spec/integration/user_can_see_ideas_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "user can see ideas" do
-  scenario "user sees all ideas" do
+  scenario "user sees all ideas", js: true do
     create(:idea, title: "super idear")
     create(:idea, title: "wowza idear")
 
     visit '/'
-
+    save_and_open_page
     expect(page).to have_content('super idear')
     expect(page).to have_content('wowza idear')
   end
